7873a504d54ef668d9efed6cbc212dbc23dade0c
Ben Johnson <baj24268@gmail.com>
2022-12-01 00:38:45 -0500

    Initial commit

---
+++ adventOfCode/Day1.cs
@@ -0,0 +1,61 @@
+﻿namespace adventOfCode
+{
+    static class Day1
+    {
+
+        public static void Run() {
+
+            string winDir = System.Environment.GetEnvironmentVariable("windir");
+
+            StreamReader reader = new StreamReader("inputDay1.txt");
+
+            int max = 0, max2 = 0, max3 = 0;
+
+            while (reader.Peek() != -1)
+            {
+
+                int sum = 0;
+
+                string line = reader.ReadLine();
+
+                while (line != null && !line.Equals(""))
+                {
+
+                    sum += Int32.Parse(line);
+                    line = reader.ReadLine();
+
+                }
+
+                if (sum > max3)
+                {
+
+                    max = max2;
+                    max2 = max3;
+                    max3 = sum;
+
+                }
+
+                else if (sum > max2)
+                {
+
+                    max = max2;
+                    max2 = sum;
+
+                }
+
+                else if (sum > max)
+                {
+
+                    max = sum;
+
+                }
+
+            }
+
+            Console.WriteLine("Largest: " + max3);
+            Console.WriteLine("Largest 3 total: " + (max + max2 + max3));
+
+        }
+
+    }
+}
---
+++ adventOfCode/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace adventOfCode
+{
+    public class start
+    {
+        private static int Main()
+        {
+
+            Day1.Run();
+
+            return 0;
+
+        }
+    }
+
+}
