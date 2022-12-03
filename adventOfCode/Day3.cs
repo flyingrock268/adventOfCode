41189ba7369804c7cfbcb08e90cd6ba0300fc2aa
Ben Johnson <baj24268@gmail.com>
2022-12-03 01:09:41 -0500

    day3 golfed
    golfed solution to day 3

--- adventOfCode/Day3.cs
+++ adventOfCode/Day3.cs
@@ -13,40 +13,11 @@ namespace adventOfCode
 
             StreamReader reader = new StreamReader("inputDay3.txt");
             int sum = 0;
-
-            while (reader.Peek() != -1) { 
-            
+            while (reader.Peek() != -1) {
                 string line = reader.ReadLine();
-                string line1 = line.Substring(0, line.Length / 2);
-                string line2 = line.Substring(line.Length / 2);
-
-                for (int i = 0; i < line1.Length; i++) {
-
-                    if (line2.Contains(line1[i])) {
-
-                        //Console.WriteLine(line1[i]);
-
-                        if (line1[i] >= 97)
-                        {
-
-                            sum += line[i] - 96;
-
-                        }
-
-                        else {
-
-                            sum += line[i] - 64 + 26;
-                        
-                        }
-
-                        break;
-                    
-                    }
-                
-                }
-            
-            }
-
+                for (int i = 0; i < line.Length / 2; i++) {
+                    if (line.Substring(line.Length / 2).Contains(line[i])) { 
+                        sum += line[i] >= 97 ? line[i] - 96 : line[i] - 64 + 26; i = line.Length;}}}
             Console.WriteLine(sum);
 
         }
@@ -56,41 +27,11 @@ namespace adventOfCode
 
             StreamReader reader = new StreamReader("inputDay3.txt");
             int sum = 0;
-
-            while (reader.Peek() != -1)
-            {
-
-                string line = reader.ReadLine();
-                string line2 = reader.ReadLine();
-                string line3 = reader.ReadLine();
-
-                for (int i = 0; i < line.Length; i++) {
-
-                    if (line2.Contains(line[i]) && line3.Contains(line[i])) {
-
-
-                        if (line[i] >= 97)
-                        {
-
-                            sum += line[i] - 96;
-
-                        }
-
-                        else
-                        {
-
-                            sum += line[i] - 64 + 26;
-
-                        }
-
-                        break;
-
-                    }
-                
-                }
-
-            }
-
+            while (reader.Peek()!=-1){
+                string line=reader.ReadLine(),line2=reader.ReadLine(),line3=reader.ReadLine();
+                for (int i=0;i<line.Length;i++) {
+                    if (line2.Contains(line[i])&&line3.Contains(line[i])) {
+                        sum += line[i] >= 97 ? line[i] - 96 : line[i] - 64 + 26; i=line.Length;}}}
             Console.WriteLine(sum);
 
         }
